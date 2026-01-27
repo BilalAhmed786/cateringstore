c8fc54133b28bb492aba1c0fb68c9e94c7c1128e
BilalAhmed786 <fasst.sallar@gmail.com>
2026-01-27 20:50:50 +0500

    creating layout for redirect

--- app/admin/layout.tsx
+++ app/admin/layout.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { useAuthRedirect } from "@/components/reusables/hooks/useAuthRedirect";
-
+import { FullScreenLoader } from "@/components/reusables/laoder/laoder";
 export default function AdminLayout({ children }: { children: React.ReactNode }) {
   const { isPending } = useAuthRedirect();
 
   if (isPending) {
-    return <p>...parent</p>
+    return <FullScreenLoader/>
   }
 
   return children;
--- app/auth/layout.tsx
+++ app/auth/layout.tsx
@@ -1,11 +1,11 @@
 "use client";
 import { useAuthRedirect } from "@/components/reusables/hooks/useAuthRedirect";
-
+import { FullScreenLoader } from "@/components/reusables/laoder/laoder";
 export default function AuthLayout({ children }: { children: React.ReactNode }) {
   const { isPending } = useAuthRedirect();
 
   if (isPending) {
-    return <p>...parent</p>
+    return <FullScreenLoader/>
   }
 
   return children;
