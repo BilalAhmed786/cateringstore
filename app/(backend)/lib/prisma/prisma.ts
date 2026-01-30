edaa9493b6c8581df76d4af28aa20a6ea3ffeb68
BilalAhmed786 <fasst.sallar@gmail.com>
2026-01-30 21:14:45 +0500

    prisma configration with new directory

--- app/(backend)/lib/prisma/prisma.ts
+++ app/(backend)/lib/prisma/prisma.ts
@@ -1,26 +1,21 @@
-// lib/prisma.ts
 import { PrismaClient } from "@prisma/client";
 import { PrismaPg } from "@prisma/adapter-pg";
-import dotenv from 'dotenv'
-dotenv.config()
+import "dotenv/config"; 
+
+
 const globalForPrisma = global as unknown as { prisma?: PrismaClient };
 
-// Create adapter instance
 const adapter = new PrismaPg({
   connectionString: process.env.DATABASE_URL!,
 });
 
-// Create Prisma client with adapter
 const prisma =
   globalForPrisma.prisma ??
   new PrismaClient({
-    adapter, // this is valid with Prisma 5+ with adapter support
-    log: ["query", "error", "warn"], // optional logs
+    adapter,
+    log: ["query", "error", "warn"],
   });
 
-// Prevent multiple instances in dev
-if (process.env.NODE_ENV !== "production") {
-  globalForPrisma.prisma = prisma;
-}
+if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = prisma;
 
 export default prisma;
--- prisma.config.ts
+++ prisma.config.ts
@@ -1,12 +1,10 @@
-import "dotenv/config";
+import "dotenv/config"; 
 import { defineConfig } from "prisma/config";
 
 export default defineConfig({
-  schema: "prisma/schema.prisma",
-  migrations: {
-    path: "prisma/migrations",
-  },
+  schema: "app/(backend)/prisma/schema.prisma",
+  migrations: { path: "app/(backend)/prisma/migrations" },
   datasource: {
-    url: process.env["DATABASE_URL"],
+    url: process.env.DATABASE_URL!, 
   },
 });
