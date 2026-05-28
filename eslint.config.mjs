97cfb196ef20f14866660a83de7f589105aacaa5
BilalAhmed786 <fasst.sallar@gmail.com>
2026-05-28 20:25:44 +0500

    refactor pacakges

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -5,9 +5,20 @@ import nextTs from "eslint-config-next/typescript";
 const eslintConfig = defineConfig([
   ...nextVitals,
   ...nextTs,
-  // Override default ignores of eslint-config-next.
+
+  {
+    rules: {
+      "@typescript-eslint/no-unused-vars": [
+        "warn",
+        {
+          varsIgnorePattern: "^_",
+          argsIgnorePattern: "^_",
+        },
+      ],
+    },
+  },
+
   globalIgnores([
-    // Default ignores of eslint-config-next:
     ".next/**",
     "out/**",
     "build/**",
